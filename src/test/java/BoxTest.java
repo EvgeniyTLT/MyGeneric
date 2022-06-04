a73aa1d298466f32f6a89f90977f06698c978458
EvgeniyTLT <Toy444@ya.ru>
2022-06-04 15:12:38 +0300

    add

--- src/test/java/BoxTest.java
+++ src/test/java/BoxTest.java
@@ -8,6 +8,9 @@ public class BoxTest {
     public void testBox() {
         Box box1 = new Box(10);
         Box box2 = new Box(20);
+        box2.setObject("fdsfds");
+        int n1 = 0;
+        int n2 = 0;
         int expected = 30;
         int result = (int) box1.getObject() + (int) box2.getObject();
         assertEquals(expected, result);
